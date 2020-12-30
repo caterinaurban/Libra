c8f27b7b9c5329fc23269c64e66384733e22f1b5
Denis Mazzucato <denismazzu@gmail.com>
2020-12-30 15:52:15 +0100

    add Symbolic3State to NON_APRON_DOMAINS

--- src/libra/abstract_domains/constants.py
+++ src/libra/abstract_domains/constants.py
@@ -1,4 +1,5 @@
 from libra.abstract_domains.deeppoly_domain import DeepPolyState
 from libra.abstract_domains.neurify_domain import NeurifyState
+from libra.abstract_domains.symbolic3_domain import Symbolic3State
 
-NON_APRON_DOMAINS = (DeepPolyState, NeurifyState)
+NON_APRON_DOMAINS = (DeepPolyState, NeurifyState, Symbolic3State)
