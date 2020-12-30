6f39edf9714752c741f532501894d568e2c1353b
Denis Mazzucato <denismazzu@gmail.com>
2020-12-30 17:06:32 +0100

    fix NON_APRON_DOMAINS inside backward analysis

--- src/libra/abstract_domains/constants.py
+++
@@ -1,5 +0,0 @@
-from libra.abstract_domains.deeppoly_domain import DeepPolyState
-from libra.abstract_domains.neurify_domain import NeurifyState
-from libra.abstract_domains.symbolic3_domain import Symbolic3State
-
-NON_APRON_DOMAINS = (DeepPolyState, NeurifyState, Symbolic3State)
