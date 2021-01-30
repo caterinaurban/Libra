a78ad336c4e6ef851a29fa44b8ae2e056d369b93
Denis Mazzucato <denismazzu@live.it>
2021-01-30 11:59:23 +0100

    add last tests settings

---
+++ src/libra/cpus_boxes_4cpu.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+##########################################
+# experiment C: different number of CPUs #
+##########################################
+
+LIBRA=$1
+
+#=======#
+# boxes #
+#=======#
+
+$1 tests/census/census.txt tests/census/20.py --domain boxes --min_lower 0 --lower 1 --upper 0 --max_upper 20 --cpu 4 | tee tests/census/logs6/census-20-boxes-4cpu.log
